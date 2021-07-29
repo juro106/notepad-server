00b16a56993fe208617360a666ecc789e8ac2181
juro106 <hiyuman@gmail.com>
2021-07-29 16:28:13 +0900

    build

--- database/db.go
+++ database/db.go
@@ -4,15 +4,29 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 var db *sql.DB
 
 func DbInit() (*sql.DB, error) {
 	var err error
-	d, err := sql.Open("mysql", "user:pass@tcp(db:3306)/note?charset=utf8mb4&parseTime&loc=Local")
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// d, err := sql.Open("mysql", "user:pass@tcp(db:3306)/note?charset=utf8mb4&parseTime&loc=Local")
+	d, err := sql.Open(
+		"mysql",
+		os.Getenv("DB_USER")+":"+
+			os.Getenv("DB_PASSWORD")+"@tcp("+
+			os.Getenv("DB_HOST")+":"+
+			os.Getenv("DB_PORT")+")/"+
+			os.Getenv("DB_NAME")+"?"+
+			os.Getenv("DB_OPTION"))
 	if err != nil {
 		log.Printf("%s\n", err)
 		log.Fatal(err)
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gofiber/fiber/v2 v2.15.0
+	github.com/joho/godotenv v1.3.0
 )
